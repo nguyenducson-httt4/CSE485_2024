845cb2e326f4376cbebc8ba836e543f71b5d63e9
nguyenducson-httt4 <nguyenson02022004@gmail.com>
2024-09-22 23:04:33 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -10,16 +10,41 @@
     <link rel="stylesheet" href="css/style_login.css">
 </head>
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
-            <div class="container-fluid">
-                <div class="h3">
-                    <a class="navbar-brand" href="#">Administration</a>
-                </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+<?php
+    include 'connect.php'; // Kết nối CSDL
+
+    // Kiểm tra kết nối
+    if ($conn->connect_error) {
+        die("Kết nối thất bại: " . $conn->connect_error);
+    }
+
+    // Đếm số lượng thể loại
+    $sql_theloai = "SELECT COUNT(ma_tloai) AS count_theloai FROM theloai";
+    $result_theloai = $conn->query($sql_theloai);
+    $count_theloai = $result_theloai ? $result_theloai->fetch_assoc()['count_theloai'] : 0;
+
+    // Đếm số lượng tác giả
+    $sql_tacgia = "SELECT COUNT(ma_tgia) AS count_tacgia FROM tacgia";
+    $result_tacgia = $conn->query($sql_tacgia);
+    $count_tacgia = $result_tacgia ? $result_tacgia->fetch_assoc()['count_tacgia'] : 0;
+
+    // Đếm số lượng bài viết
+    $sql_baiviet = "SELECT COUNT(ma_bviet) AS count_baiviet FROM baiviet";
+    $result_baiviet = $conn->query($sql_baiviet);
+    $count_baiviet = $result_baiviet ? $result_baiviet->fetch_assoc()['count_baiviet'] : 0;
+?>
+
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
+        <div class="container-fluid">
+            <div class="h3">
+                <a class="navbar-brand" href="#">Administration</a>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link active fw-bold" aria-current="page" href="./">Trang chủ</a>
@@ -37,74 +62,71 @@
                         <a class="nav-link" href="article.php">Bài viết</a>
                     </li>
                 </ul>
-                </div>
             </div>
-        </nav>
+        </div>
+    </nav>
+</header>
 
-    </header>
-    <main class="container mt-5 mb-5">
-        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
-        <div class="row">
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="" class="text-decoration-none">Người dùng</a>
-                        </h5>
-
-                        <h5 class="h1 text-center">
-                            110
-                        </h5>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="" class="text-decoration-none">Thể loại</a>
-                        </h5>
-
-                        <h5 class="h1 text-center">
-                            10
-                        </h5>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="" class="text-decoration-none">Tác giả</a>
-                        </h5>
-
-                        <h5 class="h1 text-center">
-                            20
-                        </h5>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="" class="text-decoration-none">Bài viết</a>
-                        </h5>
-
-                        <h5 class="h1 text-center">
-                            110
-                        </h5>
-                    </div>
+<main class="container mt-5 mb-5">
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card mb-2" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title text-center">
+                        <a href="" class="text-decoration-none">Người dùng</a>
+                    </h5>
+                    <h5 class="h1 text-center">
+                        155
+                    </h5>
                 </div>
             </div>
         </div>
-    </main>
-    <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
-        <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+        <div class="col-sm-3">
+            <div class="card mb-2" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title text-center">
+                        <a href="" class="text-decoration-none">Thể loại</a>
+                    </h5>
+                    <h5 class="h1 text-center">
+                        <?php echo $count_theloai; ?>
+                    </h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card mb-2" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title text-center">
+                        <a href="" class="text-decoration-none">Tác giả</a>
+                    </h5>
+                    <h5 class="h1 text-center">
+                        <?php echo $count_tacgia; ?>
+                    </h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card mb-2" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title text-center">
+                        <a href="" class="text-decoration-none">Bài viết</a>
+                    </h5>
+                    <h5 class="h1 text-center">
+                        <?php echo $count_baiviet; ?>
+                    </h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
+    <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
